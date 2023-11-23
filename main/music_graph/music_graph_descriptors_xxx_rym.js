ae1a1cb286f9eb59c1d62691c5228920c317e369
username116 <>
2023-11-23 02:24:15 +0100

    Duplicate the empty descriptor file for another graph to come

---
+++ main/music_graph/music_graph_descriptors_xxx_rym.js
@@ -0,0 +1,276 @@
+﻿'use strict';
+//22/11/23
+
+/*
+	These are the variables of the music graph: nodes (styles and genres), links, link weighting (aka distance) and rendering settings.
+	This file can be updated independently to the other files. i.e. the node/links coding (the actual graph creating) and the descriptors are separated.
+	
+	The music structure goes like this: Superclusters -> Clusters -> Supergenres -> Style clusters -> Styles (smallest nodes)
+	Obviously, the left terms are groups of the right terms.
+	
+	That means every user can set its 'own map' according to their tags. Note your files MUST be tagged according to the descriptors,
+	but you can add substitutions at style_substitutions.... that's the thing most users will have to configure according to their tag usage.
+	Note the graph functions don't care whether the tag is a genre a style or whatever tag name you use. 'Rock is rock', wherever it is. 
+	But that genre/style must be on music_graph_descriptors to be recognized.
+	
+	If you have new genres/styles not present on the graph then you probably need to add them to: 
+		- style_supergenre: that places the genre on a main big genre... For ex. Britpop into Contemporary Rock.
+		- style_cluster: that connects your new genre with related genres. For ex. R&B and Doo Wop are both in Vocal Pop style cluster.
+		- Optional:
+			- style_primary_origin: connects styles which are direct derivatives or origins. Farther than in the same style cluster.
+			- style_secondary_origin: connects styles which are secondary derivatives or origins. Farther than previous one.
+			- style_anti_influence: greatly distances two genres. It would be the opposite to being in the same style cluster.
+	
+	Now, let's say you have a group of related styles not present on the graph. For ex. Grunge Rock, Classic Grunge, etc. They are all 'grunge',
+	so you should not put them into style_supergenre matrix, where grunge already exists. We would want to add even smaller nodes than that
+	main genre. For that we use style_weak_substitutions, where we would put Grunge at the left, and connect it to Grunge Rock, Classic Grunge, etc.
+	Other approach would be to use style_cluster. Use whatever you prefer according to the link 'distance' you want to add. Values at bottom.
+		
+	'map_distance_exclusions' have those genre/style tags which are not related to an specific musical style. 
+	i.e. Acoustic could be heavy metal, rock or whatever... so we skip it (for other calcs).
+	They are filtered because they have no representation on the graph, not being a real genre/style but a musical characteristic.
+	So they are useful for similarity scoring purposes but not for the graph. 
+	This filtering stage is not needed, but it greatly speedups the calculations if you have tons of files with these tags!
+	This means than any tag not included in the graph will be omitted for calcs, but you save time if you add it manually to the exclusions (otherwise the entire graph will be visited to try to find a match).
+
+	Then we got: Primary origins, secondary origins, weak substitutions, (direct) substitutions and anti-influences.
+	The first 3 are links between styles related 'in some way'. (Direct) substitutions are equivalent  nodes (A = B), with 0 distance.
+	Finally, anti-influence accounts for styles which are considered too different, even being of the same group (style cluster or supergenres).
+	
+	The function 'musicGraph()' creates the graph, and the same for the HTML counterpart (it adds colors and all that to the main graph).
+	Execute 'Draw Graph.html' on your browser and it should load the graph set on this file. So whatever you edit here, it gets shown on the rendered version. 
+	That's an easy way to see if you added nodes at a wrong place, things not linked, etc. Much easier than checking matrices and lists of strings!
+	
+	Finally, the function 'searchByDistance()' does all the calculations for similarity between tracks.
+	
+	Tests are also available with the buttons present on these scripts (customizable button and Playlist Tools).
+*/
+const music_graph_descriptors = {
+	/*
+		----------------------------------------------------------------------------------------------------
+											Graph nodes and links											
+		----------------------------------------------------------------------------------------------------
+	*/
+	/*
+		-> Music & Supergenres clusters: Mega-Groups of related musical clusters. No need to usually touch these
+	*/
+	// Mega-Groups of related supergenre's groups: 4 big groups of popular music connected + the others
+	style_supergenre_supercluster: [
+
+	],
+	// Groups of related Supergenres
+	style_supergenre_cluster: [
+
+	],
+	/*
+		-> SuperGenres: Mega-Groups of genres and styles. Users may want to edit these at the user's descriptor file
+	*/
+	// Here genres and styles are put into their main category. Like 'Progressive Rock' and 'Hard Rock' into 'Rock&Roll Supergenre'
+	// This points to genre and styles which are considered to belong to the same parent musical genre, while not necessarily being
+	// considered 'similar' in an 'listening session' sense. For ex. 'Space Rock' and 'Southern Rock' can be considered Rock but pretty
+	// different when looking for Rock tracks. On the other hand, they are similar if you compare them to Jazz.
+	style_supergenre: [
+
+	],
+	// Small groups of related genres and styles
+	// This points to genre and styles which are usually considered pretty similar in an 'listening session' sense.
+	// For ex. instead of adding sub-styles to other places, we can add them here
+	style_cluster: [
+
+	],
+	/*
+		-> Influences: Special relations between genres and styles. Like origins, derivatives or things considered 'anti-influences'. 
+		Users may want to edit these at the user's descriptor file
+	*/
+	// Primary influence. For example one style being the origin of other.
+	// For ex. 'Rockabilly' and 'Rockabilly revival'.
+	style_primary_origin: [
+		
+	],
+	// Secondary influence. For example one style being slightly influenced by another.
+	style_secondary_origin: [
+		
+	],
+	// Anti-influences. Styles so different that are considered to be heavily distanced, even if the belong to the same genre parent.
+	// For ex. 'Americana' and 'British Folk-Rock' are both 'Folk' styles, but they are considered to be farther away than other 'Folk' styles.
+	style_anti_influence: [
+		
+	],
+	// These are genre/styles which should always apply the 'Anti-influences' filter in a listening session (see customizable button).
+	// i.e. if  a 'Jazz' track is taken as reference, 'Jazz anti-influences' should always be filtered out, because they sound 
+	// really bad together on the same listening session, even if the search of similar tracks is broadened a lot.
+	style_anti_influences_conditional: [
+		
+	],
+	/*
+		-> Substitutions: for genres and styles considered to be almost equal or even strict substitutions. 
+		Users may want to edit these at the user's descriptor file, specially to add their own substitutions
+		to avoid re-tagging their files.
+	*/
+	// Genres or styles that are pretty similar but not exactly the same. Combinations must be added as multiple entries.
+	// {A->[B,C]} EQUAL TO {A->B, A->C} BUT NOT INCLUDED {B->C}
+	style_weak_substitutions: [
+		
+	],
+	// Some big groups or clusters are equal to genres or styles 'in the classic sense', so these are direct connections for them:
+	// ALWAYS PUT FIRST the genre at the graph, then -at the right- the one(s) expected to be found on tags.
+	// Example: we tag files as 'Golden Age Rock' and/or '60s Rock' instead of 'Classic Rock' (the value at the graph), then
+	// We would add this line:
+	// ['Classic Rock XL'				,	['Golden Age Rock','6os Rock'	]],
+	// Alternatively we could change this line:
+	// ['Classic Rock XL'				,	['Classic Rock'					]],
+	// to
+	// ['Classic Rock XL'				,	['Classic Rock','Golden Age Rock','6os Rock']],
+	style_substitutions: [
+		
+	],
+	/*
+		-> Filtering: this is mostly a list of folksonomy tags which are explicitly filtered. Any value not present 
+		on 'style_supergenre' (at top) is skipped anyway for all purposes, so it's not a requisite but it makes 
+		the search faster. Users may want to edit these at the user's descriptor file, specially if they have a lot
+		of values on style or genre tags used for classification but not directly related to a genre or style. For ex:
+		'Film Score', 'Soundtrack', 'Anime Music', ...
+	*/
+	// For graph filtering
+	map_distance_exclusions: new Set([
+		
+	]),
+	/*
+		----------------------------------------------------------------------------------------------------
+											Weighting, for Graph Creation									
+				These are the weight values for graph links between styles(S) and genres(G)
+		----------------------------------------------------------------------------------------------------
+	*/
+	/* 
+		Direct: A -> B (weight applied x1)
+		Direct connections should have bigger costs since they are not accumulative
+	*/
+	primary_origin: 185, //Primary origin / Derivative x1
+	secondary_origin: 300, //Secondary origin / Derivative x1
+	weak_substitutions: 20, //Almost equal x1
+	/* 
+		Indirect: A ->( Clusters )-> B (weight applied x2 or more)
+		Note the weight is accumulative, so bigger clusters' weights add to the previous path cost
+		Ex: Style A -> Supergenre -> Supergenre Cluster -> Supergenre -> Style B
+	*/
+	cluster: 42, //Related style / genre: Southern Rock(S) -> Heartland Rock(S)
+	intra_supergenre: 100, //Traverse between the same supergenre(SG): Southern Rock(G) -> Classic Rock(SG) -> Hard Rock(G)
+	supergenre_cluster: 50, //Traverse between the same supergenre group(SG): Classic Rock(SG) -> Rock(SGG) -> Punk (SG)
+	supergenre_supercluster: 75, //Traverse between the same music group(MG): Rap(SGG)->Rhythm Music(MG)->R&B(SGG)
+	/*
+		Special:
+	*/
+	inter_supergenre: 200, //Traverse between different contiguous supergenres groups(SGG): Rock(SGG) -> Pop(SGG)
+	inter_supergenre_supercluster: 300, //Traverse between different contiguous supergenres groups(SGG): Rock(SGG) -> Pop(SGG)
+	substitutions: 0, //Direct connections (substitutions)
+	/*
+		Influences:
+	*/
+	anti_influence: 100, //backlash / anti-influence between two nodes (added directly to the total path distance): A -> ? -> B
+	primary_origin_influence: -10, //primary origin-influence between two nodes (added directly to the total path distance): A -> ? -> B
+	secondary_origin_influence: -5, //secondary origin-influence between two nodes (added directly to the total path distance): A -> ? -> B
+	/*	
+		Note on intra_supergenre:
+		-------------------------
+		Use that value as the 'basic' distance value for similar genre/styles: x3/2, x2, etc.
+		Having in mind that the max distance between 2 points on the graph will probably be ~ x4-x5 that value.
+		A lower value (cluster or 1/2) would only output the nearest or almost same genre/styles.
+		-------------------------
+		Note on anti_influence:
+		-------------------------
+		It applies to anything listed on style_anti_influence. Same logic than the rest.
+		The value is added to the total distance calculated between 2 nodes. i.e. if Rock to Jazz had a distance of 300,
+		if they had an anti-influence link, then the total distance would be 300 + 100 = 400. Being farther than before...
+		-------------------------
+		Note on primary_origin_influence (same applies to secondary_origin_influence):
+		-------------------------
+		It only applies to those nodes which have a primary origin link AND are in the same Supergenre (SG).
+		Contrary to anti_influence which applies globally and only on nodes listed in its associated array.
+		This is done to account for genres/styles which are nearer than others on the same Supergenre, 
+		while not using a style cluster or weak substitution approach.
+		Also beware of setting too high (absolute) values, the value is directly applied to the final total path distance...
+		the idea is that cluster related nodes (85) should be nearer than intra-Supergenre related nodes (100). When adding a
+		primary_origin link, then it would be omitted (being greater than the other two) but the influence applies.
+		The total distance would be 85 - 10 = 75 for cluster related nodes and 100 - 10 = 90 for intra-Supergenre related nodes.
+		But also when considering intra-Supergenre related nodes with primary_origin links (90) against cluster related nodes
+		without such link (85) the cluster related ones are still neared than the others.
+	*/
+	/*
+		----------------------------------------------------------------------------------------------------
+													For drawing 											
+				These are the weight values for graph links between styles(S) and genres(G)
+		----------------------------------------------------------------------------------------------------
+	*/
+	// Assigns colors to labels and nodes
+	// Anything named '..._supergenre' will be added to the html color label legend automatically.
+	// If more than one '...Folk..._supergenre' or '...Classical..._supergenre' is found, then it will be skipped.
+	// i.e. It will list Folk and Classical only once, even if there are multiple (sub)SuperGenres.
+	map_colors: [	// Todo: use colorbrewer sequential palettes
+		// Supergenres
+		
+		// Supergenre Clusters
+		
+		// Supergenre SuperClusters
+		
+	],
+	
+	// Attributes for every node type
+	nodeSize: 10,
+	nodeShape: 'rect', //'circle','rect','star' or 'image'. 'Image' requires 'imageLink' data for every node on drawing function
+	nodeImageLink: 'helpers-external/ngraph/html/images/Starv2.png',
+	
+	style_clusterSize: 20,
+	style_clusterShape: 'star',
+	style_clusterImageLink: 'helpers-external/ngraph/html/images/Star.png',
+	
+	style_supergenreSize: 15,
+	style_supergenreShape: 'circle',
+	style_supergenreImageLink: 'helpers-external/ngraph/html/images/Star.png',
+	
+	style_supergenre_clusterSize: 18,
+	style_supergenre_clusterShape: 'circle',
+	style_supergenre_clusterImageLink: 'helpers-external/ngraph/html/images/Star.png',
+	
+	style_supergenre_superclusterSize: 22,
+	style_supergenre_superclusterShape: 'rect',
+	style_supergenre_superclusterImageLink: 'helpers-external/ngraph/html/images/Star_color.png',
+	
+	// Other
+	bPreRender: true, // (false) Renders graph on the fly on browsers or (true) pre-rendering (it may take some time while loading entire graph)
+	renderMethod: 'realDistance'	// ('graph') Renders graph according to link centrality/gravity forces.
+									// ('graphWeighted') uses the link's weight values at top to render similar distances to real ones, but also using link forces.
+									// ('realDistance') uses the link's weight values at top to render real distances. Beware it will look really weird!
+};
+
+(function () {	// Clean non ASCII values
+	const asciiRegEx = [[/[\u0300-\u036f]/g, ''], [/\u0142/g, 'l']];
+	const asciify = (node) => {
+		let asciiNode = node.normalize('NFD');
+		asciiRegEx.forEach((rgex) => {asciiNode = asciiNode.replace(rgex[0], rgex[1]);});
+		return asciiNode;
+	};
+	['style_supergenre_supercluster', 'style_supergenre_cluster', 'style_supergenre', 'style_cluster', 'style_primary_origin', 'style_secondary_origin', 'style_weak_substitutions', 'style_substitutions'].forEach((key) => {
+		music_graph_descriptors[key].forEach((pair) => {
+			if (!pair || !Array.isArray(pair) || pair.length !== 2) {return;}
+			const parent = pair[0];
+			const asciiParent = asciify(parent);
+			if (asciiParent !== parent) {pair[0] = asciiParent;}
+			const nodeList = pair[1];
+			nodeList.forEach((node, j) => {
+				const asciiNode = asciify(node);
+				if (asciiNode !== node) {nodeList[j] = asciiNode;}
+			});
+		});
+	});
+
+	music_graph_descriptors.map_distance_exclusions.forEach((node, i, set) => {
+		const asciiNode = asciify(node);
+		if (asciiNode !== node) {set.add(asciiNode);}
+	});
+	
+	music_graph_descriptors.map_colors.forEach((pair) => {
+		const node = pair[0];
+		const asciiNode = asciify(node);
+		if (asciiNode !== node) {pair[0] = asciiNode;}
+	});
+})();
